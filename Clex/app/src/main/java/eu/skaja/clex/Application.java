f9391786a5bc050c242e8884547dd91365628c10
polest <a.jaks@gmx.de>
2017-06-07 02:28:32 +0200

    Alter Projekt mit neuem Projekt überschrieben

---
+++ Clex/app/src/main/java/eu/skaja/clex/Application.java
@@ -0,0 +1,16 @@
+package eu.skaja.clex;
+
+import ly.img.android.ImgLySdk;
+
+
+public class Application extends android.app.Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ImgLySdk.init(this);
+    }
+
+
+}
