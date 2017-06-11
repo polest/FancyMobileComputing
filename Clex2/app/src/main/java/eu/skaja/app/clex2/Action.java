3da14e9db803e8215a10e9e89acd62e75a0f498d
polest <a.jaks@gmx.de>
2017-06-11 19:10:59 +0200

    Clex2 - Bilder aus der Gallery auswählen

---
+++ Clex2/app/src/main/java/eu/skaja/app/clex2/Action.java
@@ -0,0 +1,7 @@
+package eu.skaja.app.clex2;
+
+public class Action {
+
+	public static final String ACTION_PICK = "luminous.ACTION_PICK";
+	public static final String ACTION_MULTIPLE_PICK = "luminous.ACTION_MULTIPLE_PICK";
+}
---
+++ Clex2/app/src/main/java/eu/skaja/app/clex2/CustomGallery.java
@@ -0,0 +1,8 @@
+package eu.skaja.app.clex2;
+
+public class CustomGallery {
+
+	public String sdcardPath;
+	public boolean isSeleted = false;
+
+}
