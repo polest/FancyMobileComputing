6220b457bf6d52b505fcf188a42e5169f84d2754
polest <a.jaks@gmx.de>
2017-06-15 14:47:38 +0200

    Android Gallery eingebaut

--- Clex2/app/src/main/java/eu/skaja/app/clex2/MainActivity.java
+++ Clex2/app/src/main/java/eu/skaja/app/clex2/MainActivity.java
@@ -108,8 +108,11 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 
-				Intent i = new Intent(Action.ACTION_PICK);
-				startActivityForResult(i, 100);
+				Intent intent = new Intent();
+				intent.setType("image/*");
+				intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+				intent.setAction(Intent.ACTION_GET_CONTENT);
+				startActivityForResult(Intent.createChooser(intent,"Select Picture"), 1);
 
 			}
 		});
@@ -126,7 +129,7 @@ public class MainActivity extends Activity {
 
 	}
 
-
+	
 
 
 
@@ -164,4 +167,68 @@ public class MainActivity extends Activity {
 			adapter.addAll(dataT);
 		}
 	}
+
+
+	/*
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		try {
+			// When an Image is picked
+			if (requestCode == PICK_IMAGE_MULTIPLE && resultCode == RESULT_OK
+					&& null != data) {
+				// Get the Image from data
+
+				String[] filePathColumn = { MediaStore.Images.Media.DATA };
+				imagesEncodedList = new ArrayList<String>();
+				if(data.getData()!=null){
+
+					Uri mImageUri=data.getData();
+
+					// Get the cursor
+					Cursor cursor = getContentResolver().query(mImageUri,
+							filePathColumn, null, null, null);
+					// Move to first row
+					cursor.moveToFirst();
+
+					int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+					imageEncoded  = cursor.getString(columnIndex);
+					cursor.close();
+
+				}else {
+					if (data.getClipData() != null) {
+						ClipData mClipData = data.getClipData();
+						ArrayList<Uri> mArrayUri = new ArrayList<Uri>();
+						for (int i = 0; i < mClipData.getItemCount(); i++) {
+
+							ClipData.Item item = mClipData.getItemAt(i);
+							Uri uri = item.getUri();
+							mArrayUri.add(uri);
+							// Get the cursor
+							Cursor cursor = getContentResolver().query(uri, filePathColumn, null, null, null);
+							// Move to first row
+							cursor.moveToFirst();
+
+							int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+							imageEncoded  = cursor.getString(columnIndex);
+							imagesEncodedList.add(imageEncoded);
+							cursor.close();
+
+						}
+						Log.v("LOG_TAG", "Selected Images" + mArrayUri.size());
+					}
+				}
+			} else {
+				Toast.makeText(this, "You haven't picked Image",
+						Toast.LENGTH_LONG).show();
+			}
+		} catch (Exception e) {
+			Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
+					.show();
+		}
+
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+	*/
+
+
 }
